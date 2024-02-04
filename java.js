d2b9d97b9fba9191a7e354743ca56e791272056e
Tugba <filizakca4@gmail.com>
2024-02-04 16:41:49 +0100

    updated

--- java.js
+++ java.js
@@ -76,4 +76,12 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
    
-    
+    document.addEventListener('DOMContentLoaded', function () {
+      const navbarToggle = document.getElementById('js-navbar-toggle');
+      const menu = document.getElementById('js-menu');
+  
+      navbarToggle.addEventListener('click', function () {
+          menu.classList.toggle('active');
+      });
+  });
+  
